af8c486e68bb734a9a6fd5e80693ef3f989452ce
scripka <olga.morgan17@gmail.com>
2021-01-04 16:56:00 -0700

    Add nodemon package

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3001); 
+app.locals.title = 'iVolunteer API';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`);
+})
+
